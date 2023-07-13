9b36fc6ddd31001f4719f16152af0cacb85b44aa
1395952378@qq.com <jlcchy5553>
2023-07-13 19:07:40 +0800

    提交初始消息

---
+++ 01-test/src/main/java/com/study/sjj/test/TestController.java
@@ -0,0 +1,14 @@
+package com.study.sjj.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping("test")
+    public String test(){
+        return "集成成功";
+    }
+
+}
